344ecb996e0f79fa66e32007befd3765012b9a67
Lecoultrejean <lecoultre.dev85@gmail.com>
2026-06-25 15:53:47 +0200

    ajout de git ignore

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Bienvenue</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+    return <header>Mon Header</header>;
+}
